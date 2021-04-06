15eca08ecfab11ed33ee3ba22957d7c6610ffece
divinity666 <divinity666@users.sourceforge.net>
2021-04-06 21:01:42 +0200

    increase test code coverage

--- spec/integration/annotations_table_include_processor_spec.rb
+++ spec/integration/annotations_table_include_processor_spec.rb
@@ -13,10 +13,25 @@ describe AnnotationsTableIncludeProcessor do
     @report = report
   end
 
-  it 'can be processed' do
+  it 'can be processed if panel and dashboard is given' do
     expect(@report.logger).not_to receive(:error)
-    expect(Asciidoctor.convert("include::grafana_annotations[columns=\"time,id,alertName\",panel=\"#{STUBS[:panel_sql][:id]}\",dashboard=\"#{STUBS[:dashboard]}\"]", to_file: false)).not_to include('GrafanaReporterError')
-    expect(Asciidoctor.convert("include::grafana_annotations[columns=\"time,id,alertName\",panel=\"#{STUBS[:panel_sql][:id]}\",dashboard=\"#{STUBS[:dashboard]}\"]", to_file: false)).to include('Panel Title alert')
+    expect(Asciidoctor.convert("include::grafana_annotations[columns=\"time,id,alertName\",panel=\"#{STUBS[:panel_sql][:id]}\",dashboard=\"#{STUBS[:dashboard]}\",from=\"0\",to=\"0\"]", to_file: false)).not_to include('GrafanaReporterError')
+    expect(Asciidoctor.convert("include::grafana_annotations[columns=\"time,id,alertName\",panel=\"#{STUBS[:panel_sql][:id]}\",dashboard=\"#{STUBS[:dashboard]}\",from=\"0\",to=\"0\"]", to_file: false)).to include('Panel Title alert')
+    expect(WebMock).to have_requested(:get, "http://localhost/api/annotations?dashboardId=#{STUBS[:dashboard]}&panelId=#{STUBS[:panel_sql][:id]}&from=0&to=0").twice
+  end
+
+  it 'can be processed if dashboard is given' do
+    expect(@report.logger).not_to receive(:error)
+    expect(Asciidoctor.convert("include::grafana_annotations[columns=\"time,id,alertName\",dashboard=\"#{STUBS[:dashboard]}\",from=\"0\",to=\"0\"]", to_file: false)).not_to include('GrafanaReporterError')
+    expect(Asciidoctor.convert("include::grafana_annotations[columns=\"time,id,alertName\",dashboard=\"#{STUBS[:dashboard]}\",from=\"0\",to=\"0\"]", to_file: false)).to include('Panel Title alert')
+    expect(WebMock).to have_requested(:get, "http://localhost/api/annotations?dashboardId=#{STUBS[:dashboard]}&from=0&to=0").twice
+  end
+
+  it 'can be processed without dashboard and panel' do
+    expect(@report.logger).not_to receive(:error)
+    expect(Asciidoctor.convert("include::grafana_annotations[columns=\"time,id,alertName\",from=\"0\",to=\"0\"]", to_file: false)).not_to include('GrafanaReporterError')
+    expect(Asciidoctor.convert("include::grafana_annotations[columns=\"time,id,alertName\",from=\"0\",to=\"0\"]", to_file: false)).to include('Panel Title alert')
+    expect(WebMock).to have_requested(:get, "http://localhost/api/annotations?from=0&to=0").twice
   end
 
   it 'shows error if unknown columns are specified' do
