89ebcc8b5a45158c28cb32fe3587493c24cd8b95
divinity666 <divinity666@users.sourceforge.net>
2021-05-17 10:20:10 +0200

    update version information

--- lib/VERSION.rb
+++ lib/VERSION.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 # Version information
-GRAFANA_REPORTER_VERSION = [0, 4, 0].freeze
+GRAFANA_REPORTER_VERSION = [0, 4, 1].freeze
 # Release date
-GRAFANA_REPORTER_RELEASE_DATE = '2021-04-14'
+GRAFANA_REPORTER_RELEASE_DATE = '2021-05-17'
