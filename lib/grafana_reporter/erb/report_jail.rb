22681ab9fa21bb887fce8ec0e640ed3396c85fb3
divinity666 <divinity666@users.sourceforge.net>
2021-06-06 14:07:53 +0200

    remove unneeded comment

--- lib/grafana_reporter/erb/report_jail.rb
+++ lib/grafana_reporter/erb/report_jail.rb
@@ -7,7 +7,6 @@ module GrafanaReporter
     class ReportJail
       attr_reader :report, :attributes
 
-      # TODO: check if attributes need to become GrafanaVariable objects
       def initialize(report, attributes)
         @report = report
         @attributes = attributes
