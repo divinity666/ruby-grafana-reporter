87fa63c97860215dc3f00d55f7ca47b5b0cd8720
divinity666 <divinity666@users.sourceforge.net>
2021-12-16 13:52:32 +0100

    add missing blank to error message

--- lib/grafana_reporter/errors.rb
+++ lib/grafana_reporter/errors.rb
@@ -65,7 +65,7 @@ module GrafanaReporter
   # Details about how to fix that are provided in the message.
   class ConfigurationDoesNotMatchSchemaError < ConfigurationError
     def initialize(item, verb, expected, currently)
-      super("Configuration file does not match schema definition. Expected '#{item}' to #{verb} '#{expected}',"\
+      super("Configuration file does not match schema definition. Expected '#{item}' to #{verb} '#{expected}', "\
             "but was '#{currently}'.")
     end
   end
