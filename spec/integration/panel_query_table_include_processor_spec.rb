f97083fef426e73237c2efb2323faae80a2790f6
divinity666 <divinity666@users.sourceforge.net>
2022-07-31 20:52:31 +0200

    add support for prometheus queries with POST interface, which has been introduced since grafana 7.5.x

--- spec/integration/panel_query_table_include_processor_spec.rb
+++ spec/integration/panel_query_table_include_processor_spec.rb
@@ -128,10 +128,23 @@ describe PanelQueryTableIncludeProcessor do
   end
 
   context 'prometheus' do
+    after(:each) do
+      @report.grafana("default").instance_eval("@version = '#{STUBS[:version]}'")
+    end
+
     it 'can handle prometheus requests' do
       @report.logger.level = ::Logger::Severity::DEBUG
       allow(@report.logger).to receive(:debug)
       expect(@report.logger).to receive(:debug).with(/^Requesting .*&step/)
+      expect(@report.logger).not_to receive(:error)
+      expect(Asciidoctor.convert("include::grafana_panel_query_table:#{STUBS[:panel_prometheus][:id]}[query=\"#{STUBS[:panel_prometheus][:letter]}\",dashboard=\"#{STUBS[:dashboard]}\",from=\"0\",to=\"0\"]", to_file: false)).to match(/<p>\| 1617728730 \|  \|  \|  \| 0.011986814503580401 \| 0.6412945761450544\n\|/)
+    end
+
+    it 'can handle new prometheus requests since grafana 7.5.x' do
+      @report.grafana("default").instance_eval("@version = '7.5.3'")
+
+      @report.logger.level = ::Logger::Severity::DEBUG
+      allow(@report.logger).to receive(:debug)
       expect(@report.logger).not_to receive(:error)
       expect(Asciidoctor.convert("include::grafana_panel_query_table:#{STUBS[:panel_prometheus][:id]}[query=\"#{STUBS[:panel_prometheus][:letter]}\",dashboard=\"#{STUBS[:dashboard]}\",from=\"0\",to=\"0\"]", to_file: false)).to match(/<p>\| 1617728730 \|  \|  \|  \| 0.011986814503580401 \| 0.6412945761450544\n\|/)
     end
