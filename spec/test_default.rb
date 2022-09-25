ef824b31df3cf976ab9ab4eb341902c37e0f83c6
divinity666 <divinity666@users.sourceforge.net>
2022-09-25 20:16:58 +0200

    coveralls can send data to cloud again (done by monkey-patching) remove unneeded coveralls.yml deploys enabled only on tagged commits

--- spec/test_default.rb
+++ spec/test_default.rb
@@ -1,5 +1,29 @@
-if ENV['APPVEYOR']
+if ENV['COVERALLS_REPO_TOKEN']
   require 'coveralls'
+
+  # FIXME monkey-patch coveralls, as this is needed for coveralls API calls from appveyor
+  module Coveralls
+    class API
+
+      class << self
+        alias_method :build_request_orig, :build_request
+      end
+
+      private
+
+      def self.build_request(path, hash)
+        request = self.build_request_orig(path, hash)
+        if request.content_type.include?(",")
+          Coveralls::Output.puts("[ruby-grafana-reporter] monkey patching Coveralls::API request", :color => "yellow")
+          request.content_type = request.content_type.gsub(/,/, ";")
+        else
+          Coveralls::Output.puts("[ruby-grafana-reporter] monkey patching Coveralls::API no longer needed and may be REMOVED", :color => "yellow")
+        end
+        request
+      end
+    end
+  end
+
   Coveralls.wear! do
     add_filter "spec/"
   end
