49392417cb2b022dfa5492a2bcc1c084eedf8052
divinity666 <divinity666@users.sourceforge.net>
2024-06-08 22:20:29 +0200

    prepare release

--- lib/VERSION.rb
+++ lib/VERSION.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 # Version information
-GRAFANA_REPORTER_VERSION = [0, 7, 0].freeze
+GRAFANA_REPORTER_VERSION = [0, 8, 0].freeze
 # Release date
-GRAFANA_REPORTER_RELEASE_DATE = '2024-05-19'
+GRAFANA_REPORTER_RELEASE_DATE = '2024-06-08'
