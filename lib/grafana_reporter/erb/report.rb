d1bd8be563536714c5a847af137d1868b5358099
asapals@onairent.live <asapals@onairent.live>
2024-06-10 22:10:30 +0300

    Implementing compression for erb generated reports

--- lib/grafana_reporter/erb/report.rb
+++ lib/grafana_reporter/erb/report.rb
@@ -7,7 +7,7 @@ module GrafanaReporter
     # Implementation of a specific {AbstractReport}. It is used to
     # build reports specifically for erb templates.
     class Report < ::GrafanaReporter::AbstractReport
-      # Starts to create an asciidoctor report. It utilizes all extensions in the {GrafanaReporter::Asciidoctor}
+      # Starts to create an erb report. It utilizes all extensions in the {GrafanaReporter::ERB}
       # namespace to realize the conversion.
       # @see AbstractReport#build
       def build
@@ -15,6 +15,25 @@ module GrafanaReporter
         logger.debug("Document attributes: #{attrs}")
 
         File.write(path, ::ERB.new(File.read(@template)).result(ReportJail.new(self, attrs).bind))
+
+        # build zip file
+        zip_file = Tempfile.new('gf_zip')
+        buffer = Zip::OutputStream.write_buffer do |zipfile|
+          # add report file
+          zipfile.put_next_entry("#{path.gsub(@config.reports_folder, '')}.#{@config.report_class.default_result_extension}")
+          zipfile.write File.read(path)
+        end
+        File.open(zip_file, 'wb') do |f|
+          f.write buffer.string
+        end
+
+        # replace original file with zip file
+        zip_file.rewind
+        begin
+          File.write(path, zip_file.read)
+        rescue StandardError => e
+          logger.fatal("Could not overwrite report file '#{path}' with ZIP file. (#{e.message}).")
+        end
       end
 
       # @see AbstractReport#default_template_extension
