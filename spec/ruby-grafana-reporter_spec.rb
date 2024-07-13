1cf75907ab395385c37682af9166bc3592aa4093
divinity666 <divinity666@users.sourceforge.net>
2024-07-13 12:47:09 +0200

    print, RSpec test results in plain text, to easily see, where it fails in pipeline

--- spec/ruby-grafana-reporter_spec.rb
+++ spec/ruby-grafana-reporter_spec.rb
@@ -1,3 +1,10 @@
 require_relative 'stubs/webmock'
+
+RSpec.configure do |config|
+  config.color = true
+  config.tty = true
+  config.formatter = :documentation
+end
+
 Dir["spec/models/*.rb"].each {|f| require_relative "../#{f}"}
 Dir["spec/integration/*.rb"].each {|f| require_relative "../#{f}"}
