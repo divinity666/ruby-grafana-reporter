5d9ee14c2948d08f9c7fde2f6a5523c5e747fadf
divinity666 <divinity666@users.sourceforge.net>
2024-08-04 15:16:17 +0200

    add dependencies explicitly for OCRAN

--- lib/ruby_grafana_reporter.rb
+++ lib/ruby_grafana_reporter.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'rubygems'
-require 'rubygems/name_tuple'
-require 'rubygems/specification_policy'
-require 'rubygems/ext'
+require 'rubygems' # for OCRAN
+require 'rubygems/name_tuple' # for OCRAN
+require 'rubygems/specification_policy' # for OCRAN
+require 'rubygems/ext' # for OCRAN
 require 'net/http'
 require 'fileutils'
 require 'yaml'
@@ -17,7 +17,8 @@ require 'date'
 require 'time'
 require 'logger'
 require 'asciidoctor'
-require 'asciidoctor/extensions'
+require 'asciidoctor/extensions' # for OCRAN
+require 'asciidoctor/converter/html5' # for OCRAN
 require 'asciidoctor-pdf'
 require 'zip'
 require_relative 'VERSION'
